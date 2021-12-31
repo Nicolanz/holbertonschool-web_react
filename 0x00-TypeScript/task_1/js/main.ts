b3d9c4cb2b0da217380b3117630fdf11e2c1dc53
Nicolanz <zarateguarinnicolasandres@gmail.com>
2021-12-31 16:03:06 -0500

    interfaces tasks

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -8,6 +8,19 @@ interface Teacher {
     location: string;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstname: string, lastName: string): string;
+}
+
+// objs and functionality examples
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -15,5 +28,15 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+let printnewTeacher: printTeacherFunction = printTeacher;
 
 console.log(teacher3);
+console.log(director1);
+console.log(printnewTeacher("Nicolas", "Zarate"));
